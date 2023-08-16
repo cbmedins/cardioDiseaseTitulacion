8e3e347f88183a1c4a25012a400c8bd7e6fa5418
Cristhian <cbmedins@gmail.com>
2023-08-16 00:04:23 -0500

    init

--- components/layout/user-dropdown.tsx
+++ components/layout/user-dropdown.tsx
@@ -25,7 +25,7 @@ export default function UserDropdown({ session }: { session: Session }) {
               <button
               className="relative flex w-full items-center justify-start space-x-2 rounded-md p-2 text-left text-sm transition-all duration-75 hover:bg-gray-100"
             >
-              <Link href="/dashboard">
+              <Link href="dashboard">
                 <a className="flex items-center justify-start space-x-2 relative w-full rounded-md p-2 text-left text-sm transition-all duration-75 hover:bg-gray-100">
                   <LayoutDashboard className="h-4 w-4" />
                   <p className="text-sm">Dashboard</p>
